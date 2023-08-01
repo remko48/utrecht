9c9b2f7a5d941a7c1e637f87e662bb82ba321ff1
Bryan de Jong <bryandejong99@gmail.com>
2023-08-01 09:38:09 +0100

    fixup! feat: add vega react component

--- packages/component-library-react/src/VegaVisualisation.tsx
+++ packages/component-library-react/src/VegaVisualisation.tsx
@@ -1,11 +1,10 @@
 import clsx from 'clsx';
-import { PropsWithChildren } from 'react';
 import { Vega } from 'react-vega';
 import { VegaProps } from 'react-vega/lib/Vega';
 
 export interface VegaVisualisationProps extends VegaProps {}
 
-export const VegaVisualisation = ({ className, ...restProps }: PropsWithChildren<VegaVisualisationProps>) => (
+export const VegaVisualisation = ({ className, ...restProps }: VegaVisualisationProps) => (
   <Vega {...restProps} className={clsx('utrecht-vega-visualisation', className)} />
 );
 
